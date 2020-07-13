c2ca1e020f0f1eb52e38315aeed5e70d0591f223
Zulfat Miftahutdinov <zulfatmi@gmail.com>
2020-07-13 17:14:54 +0300

    ff

--- data_processing_utils/combine.py
+++ data_processing_utils/combine.py
@@ -1,13 +1,42 @@
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument('--test_labels')
+parser.add_argument('--gold_data')
 parser.add_argument('--predicted')
 parser.add_argument('--save_to')
 args = parser.parse_args()
 
-with open(args.test_labels, encoding='utf-8') as input_stream:
-  test_labels = [line.strip() for line in input_stream]
+def iterate_over_conll(file_path, sep=' '):
+    with open(file_path, encoding="utf-8") as f:
+        words = []
+        labels = []
+        gazetteers = []
+        for line in f:
+            if line.startswith("-DOCSTART-") or line == "" or line == "\n":
+                if words:
+                    yield words, labels, gazetteers
+                    words = []
+                    labels = []
+                    gazetteers = []
+            else:
+                splits = line.split(sep)
+                #replacing spaces with underscore in words
+                words.append(splits[0].replace(' ', '_'))
+                if len(splits) > 1:
+                    labels.append(splits[1].replace("\n", ""))
+                else:
+                    # Examples could have no label for mode = "test"
+                    labels.append("O")
+                if len(splits) > 2:
+                    gazetteers.append(splits[2].replace("\n", ""))
+                else:
+                    gazetteers.append("O")
+        if words:
+            yield words, labels, gazetteers
+
+test_labels = []
+for words, labels, gazetteers in iterate_over_conll(args.gold_data, sep='\t'):
+  test_labels.append(labels)
 
 pred_data = [[]]
 with open(args.predicted, encoding='utf-8') as input_stream:
@@ -22,6 +51,6 @@ print(len(pred_data), len(test_labels))
 assert len(test_labels) == len(pred_data)
 with open(args.save_to, 'w', encoding='utf-8') as output_stream:
   for pred_token_tags, true_tags in zip(pred_data, test_labels):
-    for pred_toke_tag, true_tag in zip(pred_token_tags, true_tags.split()):
+    for pred_toke_tag, true_tag in zip(pred_token_tags, true_tags):
       output_stream.write(pred_toke_tag + ' ' + true_tag + '\n')
     output_stream.write('\n')
